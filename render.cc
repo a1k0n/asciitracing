b40991c315c296b625dcb86d0c14ec142ae71328
Andy Sloane <andy@a1k0n.net>
2012-10-09 22:58:37 -0700

    gamma corrected color selection.
    now the dithering picks all kinds of weird complementary colors that add
    to grey, but it doesn't look bad from far enough away...  Overall color
    reproduction is better, for sure.

--- render.cc
+++ render.cc
@@ -8,53 +8,45 @@ const int bayer_matrix[] = {
    4,12, 2,10,
   16, 8,14, 6};
 
+// get gamma-corrected intensity of a pixel value
+float gamma(float x) {
+  return pow(x*(1.0/255.0), 2.2);
+}
+
 void nearestcolor(const vec3& c, int *fg, int *bg) {
   // brute force
-  float minerr = 65536*30;
+  float minerr = 1e30;
   *fg = -1; *bg = -1;
   for(int i = 0; i < 17; i++) {
-#if 1
     for(int j = 0; j < 9; j++) {
-      float er = 0.7*palette_[j*3] + 0.3*palette_[i*3] - 255*c.x;
-      float eg = 0.7*palette_[j*3+1] + 0.3*palette_[i*3+1] - 255*c.y;
-      float eb = 0.7*palette_[j*3+2] + 0.3*palette_[i*3+2] - 255*c.z;
-      float vr = palette_[j*3] - palette_[i*3];
-      float vg = palette_[j*3+1] - palette_[i*3+1];
-      float vb = palette_[j*3+2] - palette_[i*3+2];
-#if 1
+      float er = 0.7*gamma(palette_[j*3  ]) + 0.3*gamma(palette_[i*3  ]) - c.x;
+      float eg = 0.7*gamma(palette_[j*3+1]) + 0.3*gamma(palette_[i*3+1]) - c.y;
+      float eb = 0.7*gamma(palette_[j*3+2]) + 0.3*gamma(palette_[i*3+2]) - c.z;
+      float vr = gamma(palette_[j*3  ]) - gamma(palette_[i*3  ]);
+      float vg = gamma(palette_[j*3+1]) - gamma(palette_[i*3+1]);
+      float vb = gamma(palette_[j*3+2]) - gamma(palette_[i*3+2]);
+#if 0
       float ey =  0.257*er + 0.504*eg + 0.098*eb;
       float ev =  0.439*er - 0.368*eg - 0.071*eb;
       float eu = -0.148*er - 0.291*eg + 0.439*eb;
-      int err = ey*ey + 2*ev*ev + 2*eu*eu;
+      float err = ey*ey + ev*ev + eu*eu;
+#if 0
+      err += 0.001*(vr*vr + vg*vg + vb*vg);
+#endif
+#if 0
       float vy =  0.257*vr + 0.504*vg + 0.098*vb;
       float vv =  0.439*vr - 0.368*vg - 0.071*vb;
       float vu = -0.148*vr - 0.291*vg + 0.439*vb;
-      //err += 0.1*(vr*vr + vu*vu + vb*vb);
+      err += 0.01*(vu*vu + vb*vb);
+#endif
 #else
-      int err = er*er + eg*eg + eb*eb;
-#if 0
-      err += 0.3*(vr*vr + vg*vg + vb*vg);
-#endif
-#endif
-#if 0
-      float vr = palette_[j*3] - palette_[i*3];
-      float vg = palette_[j*3+1] - palette_[i*3+1];
-      float vb = palette_[j*3+2] - palette_[i*3+2];
-      err += 0.3*(vr*vr + vg*vg + vb*vg);
+      float err = er*er + eg*eg + eb*eb;
+      err += 0.001*(vr*vr + vg*vg + vb*vg);
 #endif
       if (err < minerr) { minerr = err; *fg = i-1; *bg = j-1; }
-#else
-    float er = palette_[i*3] - 255*c.x;
-    float eg = palette_[i*3+1] - 255*c.y;
-    float eb = palette_[i*3+2] - 255*c.z;
-    int err = er*er + eg*eg + eb*eb;
-#endif
-      if (err < minerr) { minerr = err; *fg = i-1; }
-#if 1
     }
-#endif
   }
-  //printf("minerr=%d besti=%d c=%g,%g,%g\n", minerr, besti, c.x, c.y, c.z);
+  //printf("minerr=%g best=%d,%d c=%g,%g,%g\n", minerr, *fg, *bg, c.x, c.y, c.z);
 }
 
 int color_LUT[16*16*16];
@@ -77,7 +69,7 @@ void render_init() {
 
 // nearest color fg/bg, dithering offset x,y
 void render_color(const vec3& c, int x, int y) {
-  float o = bayer_matrix[(x&3) + (y&3)*4]*(1.0/8.0) - 0.5;
+  float o = bayer_matrix[(x&3) + (y&3)*4]*(1.0/8.0);
   int r = std::max(0, std::min(15, (int) (c.x*15.0 - o)));
   int g = std::max(0, std::min(15, (int) (c.y*15.0 - o)));
   int b = std::max(0, std::min(15, (int) (c.z*15.0 - o)));
